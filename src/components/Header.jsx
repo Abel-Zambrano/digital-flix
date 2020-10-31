bf333e2c745f49b5c93a25cf68b7e25515398c36
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-31 15:39:44 -0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className='header'>
-            <h1><i class="fas fa-file-video"></i> Digital Flix</h1>
+            <h1><i className="fas fa-file-video"></i> Digital Flix</h1>
         </div>
     );
 }

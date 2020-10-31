4dacb89216dc020a3ed437ee894b8bd8df2bad6c
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-31 11:14:49 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [ movies, setMovies ] = useState([]);
+
+  const access = 'b495b08d'
+
+  useEffect(() => {
+    axios.get(`http://www.omdbapi.com/?apikey=${access}&t=star+wars`)
+    .then(res => {
+      setMovies(res.data)
+    })
+  }, []);
+
+  console.log(movies);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Digital Flix</h1>
+      <div>
+        <h2>{movies.Title}</h2>
+        <h5>{movies.Plot}</h5>
+        <img src={movies.Poster} alt="" />
+      </div>
     </div>
   );
 }
